1c54d8a50800b9ee29a894276fa631c95cbb3b7c
Stan <m3chpoet@gmail.com>
2024-02-26 20:30:46 +0200

    facebook test

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -71,7 +71,7 @@ function ChatBot() {
 
     setIsLoading(true);
 
-    const openAIQuestion = `Based on the customer request: "${question}", what fields should we fetch from the Facebook Ads API? Response as field1,field2,field3,insights.fields(field1,field2,field3)`;
+    const openAIQuestion = `Based on the customer request: "${question}", what fields should we fetch from the Facebook Ads API? Always add campaign_name to the response. Only use real fields from Meta Business API. Answer as field1,field2,field3,insights.fields(field1,field2,field3)`;
 
     const messagesPayload = [
       {
