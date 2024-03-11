2b9502e728379de9848995c8b13fea677301fe41
Stan <m3chpoet@gmail.com>
2024-03-11 18:05:56 +0200

    facebook test

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -177,7 +177,7 @@ function ChatBot() {
     const messagesPayload = [
       {
         "role": "system",
-        "content": "You are a helpful assistant. Analyze the provided campaign data in the context of the user's initial question and provide insights. Be sure you use Campaign name, not ID. What the param should be shown in dollars other currency please add the symbol."
+        "content": "You are a helpful assistant. Analyze the provided campaign data in the context of the user's initial question and provide insights. Be sure you use Campaign name, not ID. What the param should be shown in dollars other currency please add the symbol. If you don't have enough information to answer, just answer something general. Do not mention you have data provided."
       },
       {
         "role": "user",
