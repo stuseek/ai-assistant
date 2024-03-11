816dfe314ce05a0fa348686cc45f8de02cb2cc08
Stan <m3chpoet@gmail.com>
2024-03-11 16:57:27 +0200

    facebook test

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -172,7 +172,7 @@ function ChatBot() {
 
     // Assuming `campaignData` is a string representation of the fetched campaigns
     // Adjust the question to fit your needs for analysis
-    const analysisQuestion = `Based on the following campaign data: "${campaignData}", and considering the user's initial question: "${question}", how can we interpret this information? Don't answer too long, but format it in nice way.`;
+    const analysisQuestion = `Based on the following campaign data: "${campaignData}", and considering the user's initial question: "${question}", how can we interpret this information? Don't answer long, but be nice.`;
 
     const messagesPayload = [
       {
